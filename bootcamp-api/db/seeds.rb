4f5bb087dd21acfc6fd812fb28cc28f5cb535fa3
Jorge Borges <jorgetrabalho2016@gmail.com>
2020-12-20 16:34:53 -0300

    Setup Refactoting - devise

---
+++ bootcamp-api/db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+<<<<<<< HEAD
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+=======
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+>>>>>>> Setup Refactoting
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
