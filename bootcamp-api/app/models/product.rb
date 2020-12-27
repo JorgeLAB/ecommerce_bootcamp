bcd133696aede79baffd9f2283c01ecb89c8aa42
Jorge Borges <jorgetrabalho2016@gmail.com>
2020-12-27 11:24:36 -0300

    Adding the status attribute to the products model.

--- bootcamp-api/app/models/product.rb
+++ bootcamp-api/app/models/product.rb
@@ -4,10 +4,13 @@ class Product < ApplicationRecord
 	has_many :categories, through: :product_categories
 	has_one_attached :image # or has_many_attached if you can more than 1 image to the product
   
+  validates :status, presence: true
   validates :name, presence: true, uniqueness: {case_sensitive: false}
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0}
   validates :image, presence: true
+
+  enum status: { available: 1, unavailable: 2 }  
 
   include NameSearchable
   include Paginatable
