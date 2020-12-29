28da34993c54c6c9602a3483c3eeb4d39d3b6255
Jorge Borges <jorgetrabalho2016@gmail.com>
2020-12-29 17:17:52 -0300

    Migration changes.

--- bootcamp-api/db/migrate/20201221192340_create_games.rb
+++ bootcamp-api/db/migrate/20201221192340_create_games.rb
@@ -2,6 +2,7 @@ class CreateGames < ActiveRecord::Migration[6.0]
   def change
     create_table :games do |t|
       t.integer :mode
+      t.string :developer
       t.datetime :release_date
       t.references :system_requirement, null: false, foreign_key: true
 
--- bootcamp-api/db/migrate/20201221194100_add_developer_to_games.rb
+++
@@ -1,5 +0,0 @@
-class AddDeveloperToGames < ActiveRecord::Migration[6.0]
-  def change
-    add_column :games, :developer, :string
-  end
-end
