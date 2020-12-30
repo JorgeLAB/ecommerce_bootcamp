f5f24a6349b79173d91940a3558974a256296604
Jorge Borges <jorgetrabalho2016@gmail.com>
2020-12-30 08:48:15 -0300

    Factory setting so that when a product is created, the productable passed by parameter is not overwritten.

--- bootcamp-api/db/seeds.rb
+++ bootcamp-api/db/seeds.rb
@@ -1,9 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-<<<<<<< HEAD
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-=======
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
->>>>>>> Setup Refactoting
 #
 # Examples:
 #
