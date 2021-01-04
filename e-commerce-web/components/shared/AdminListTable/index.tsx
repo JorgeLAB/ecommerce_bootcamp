d7dbbb8f0b90adb531b6ab1501adf2522657efc8
Jorge Borges <jorgetrabalho2016@gmail.com>
2021-01-04 11:12:19 -0300

    Implementing a component table that fits according to the passed entity. The table has customized fields. TODO: The pagination fields was inserted without funtionality.

---
+++ e-commerce-web/components/shared/AdminListTable/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styles from '../../../styles/AdminPainel.module.css';
+import { Table } from 'react-bootstrap';
+import StyledButton from '../StyledButton';
+
+interface AdminListTableProps {
+  first_title: String;
+  second_title?: String;
+  third_title?: String;
+  fourth_title?: String;
+  fifth_title?: String;
+  sixth_title?: String;
+}
+
+
+const AdminListTable: React.FC<AdminListTableProps> = ({ children, first_title, second_title, third_title, fourth_title, fifth_title, sixth_title }) => {
+  return (
+    <>
+    <div className={styles.admin_painel}>
+      <Table borderless={true} hover={true} responsive={true}>
+        <thead>
+          <tr>
+            { first_title && <th>{first_title}</th> } 
+            { second_title && <th>{second_title}</th> }
+            { third_title && <th>{third_title}</th> }
+            { fourth_title && <th>{fourth_title}</th> }
+            { fifth_title && <th>{fifth_title}</th> }
+            { sixth_title && <th>{sixth_title}</th> }
+            <th colSpan={2}> Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          { children }
+        </tbody>
+      </Table>
+    </div>
+    
+    <div className="pagination justify-content-end">
+        <div className="pagination">
+            <StyledButton action="<" type_button="blue" />
+            <StyledButton action="1" type_button="blue" />
+            <StyledButton action="2" type_button="blue" />
+            <StyledButton action="3" type_button="blue" />
+            ...
+            <StyledButton action="31" type_button="blue" />
+            <StyledButton action=">" type_button="blue" />
+        </div>
+    </div>
+    
+    </>
+    )
+}
+
+export default AdminListTable;
