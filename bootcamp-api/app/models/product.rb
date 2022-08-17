efa9aeca601180c192c720e6675cc5535739e104
Jorge Borges <jorgetrabalho2016@gmail.com>
2022-08-17 13:04:28 -0300

    Refactoring product model

--- bootcamp-api/app/models/product.rb
+++ bootcamp-api/app/models/product.rb
@@ -1,14 +1,14 @@
 class Product < ApplicationRecord
   belongs_to :productable, polymorphic: true
   has_many :product_categories, dependent: :destroy
-	has_many :categories, through: :product_categories
-	has_one_attached :image # or has_many_attached if you can more than 1 image to the product
-  
-  validates :status, presence: true
-  validates :name, presence: true, uniqueness: {case_sensitive: false}
-  validates :description, presence: true
-  validates :price, presence: true, numericality: { greater_than: 0}
-  validates :image, presence: true
+  has_many :categories, through: :product_categories
+  has_one_attached :image # or has_many_attached if you can more than 1 image to the product
+
+  with_options presence: true do
+    validates :status, :description, :image
+    validates :name, uniqueness: { case_sensitive: false }
+    validates :price, numericality: { greater_than: 0}
+  end
 
   enum status: { available: 1, unavailable: 2 }  
 
