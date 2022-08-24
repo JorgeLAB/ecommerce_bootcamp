a1fa14570784ba4438f6fa489516805cfb82ea18
Jorge Borges <jorgetrabalho2016@gmail.com>
2022-08-24 19:55:53 -0300

    Correct params name

--- bootcamp-api/spec/shared_examples/like_searchable_concern_example.rb
+++ bootcamp-api/spec/shared_examples/like_searchable_concern_example.rb
@@ -1,9 +1,9 @@
-shared_examples "like searchable concern" do | factory_name, field |
+shared_examples "like searchable concern" do | factory, field |
 	let!(:search_param) { 'Example' }
 	let!(:records_to_find) do
-		(0..3).to_a.map { |index| create(factory_name, field => "Example #{index}") }
+		(0..3).to_a.map { |index| create(factory, field => "Example #{index}") }
 	end
-	let!(:records_to_ignore){ create_list(factory_name, 3) }
+	let!(:records_to_ignore){ create_list(factory, 3) }
 
 	it "found records with expression in :name" do
 		found_records = described_class.like(field, search_param)
