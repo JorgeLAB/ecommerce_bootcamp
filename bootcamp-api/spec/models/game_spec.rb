f7088d9cee33c3ef12524e72fcf988b81fede90a
Jorge Borges <jorgetrabalho2016@gmail.com>
2022-08-24 19:59:17 -0300

    Correct testes

--- bootcamp-api/spec/models/game_spec.rb
+++ bootcamp-api/spec/models/game_spec.rb
@@ -7,4 +7,6 @@ RSpec.describe Game, type: :model do
   it { is_expected.to belong_to :system_requirement }
   it { is_expected.to belong_to(:license).optional }
   it { is_expected.to have_one :product}
+
+  it_behaves_like 'like searchable concern', :game, :developer
 end
--- bootcamp-api/spec/models/system_requirement_spec.rb
+++ bootcamp-api/spec/models/system_requirement_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe SystemRequirement, type: :model do
 	it { should validate_presence_of(:operational_system) }
 	it { should validate_presence_of(:processor) }
 
-	it_behaves_like "name searchable concern", :system_requirement
+	it_behaves_like "like searchable concern", :system_requirement, :name
 	it_behaves_like "paginatable concern", :system_requirement
 
 end
